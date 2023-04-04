266b845a867b01f259630120f61d521946ce4f78
Dimriu <dimriudobrunin@gmail.com>
2023-04-04 23:04:02 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        arr[3] = 11;
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
